1844d45d58304ed8610cc875be318ca214f4b5a5
unknown <toegelanna@outlook.com>
2019-05-25 17:23:56 +0200

    copied the initial task to Github

---
+++ Week0/annto_task1/README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at https://codepen.io/AnnTo/pen/BELZEo.
+
+ Task1 for Foo
---
+++ Week0/annto_task1/js/index.js
@@ -0,0 +1,3 @@
+document.getElementsByTagName("h1")[0].style.fontSize = "90px";
+document.getElementsByTagName("h3")[0].style.fontSize = "30px";
+document.getElementsByTagName("p")[0].style.fontSize = "20px";
